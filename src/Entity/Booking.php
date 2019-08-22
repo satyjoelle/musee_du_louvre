62fb88fb9a5d6b85e80c7e3fa7bb8a28c6a7d0c7
jojo <faveurextra@gmail.com>
2019-08-22 23:31:34 +0200

    tarif, checkout mail

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BookingRepository")
@@ -20,6 +21,7 @@ class Booking
 
     /**
      * @ORM\Column(type="date")
+     * @Assert\GreaterThan("today")
      */
     private $jour_de_visite;
 
